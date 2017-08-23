7e049c7cfc82c2210ecdaa4a858a75c866140aba
Joanne Dority <jdority@c1mtg1wdh3qk.internal.salesforce.com>
2017-08-23 12:02:05 -0700

    pushAccount for Classic + LEX

---
+++ src/aura/pushAccount/pushAccountHelper.js
@@ -0,0 +1,12 @@
+({
+    validateAccountForm: function(component) {
+        var validAccount = true;
+          console.log('validateAccountForm ' );
+        /*
+        Duplicate check logic would go here
+        
+        */
+        return(validAccount);
+	}
+    }
+})
